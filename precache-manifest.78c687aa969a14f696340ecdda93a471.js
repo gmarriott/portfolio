3f1b593c906dac9f669cd0d7c865792df6394e9c
George Marriott <george@tutora.co.uk>
2020-04-04 13:11:02 +0100

    Updates

---
+++ precache-manifest.78c687aa969a14f696340ecdda93a471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2102fe9968e7fea533c1",
+    "url": "/portfolio/static/css/main.4fc5f8c4.chunk.css"
+  },
+  {
+    "revision": "2102fe9968e7fea533c1",
+    "url": "/portfolio/static/js/main.2102fe99.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "87fb767dcdf0acdd0d4a",
+    "url": "/portfolio/static/js/2.87fb767d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1250045f12fd495f31312861084c7461",
+    "url": "/portfolio/index.html"
+  }
+];

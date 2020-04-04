74cd796b23b0c3557715bc067171e726317d70d2
George Marriott <george@tutora.co.uk>
2020-04-04 14:49:49 +0100

    Updates

--- precache-manifest.7ac41d015775e0962e9c8126c7b9471c.js
+++ precache-manifest.7ac41d015775e0962e9c8126c7b9471c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b7f7dd3aaf15a6ca93d7",
+    "revision": "63a2bb988077b96ed09e",
     "url": "/portfolio/static/css/main.cacbe3b9.chunk.css"
   },
   {
-    "revision": "b7f7dd3aaf15a6ca93d7",
-    "url": "/portfolio/static/js/main.b7f7dd3a.chunk.js"
+    "revision": "63a2bb988077b96ed09e",
+    "url": "/portfolio/static/js/main.63a2bb98.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "6722d7241fe341449ea7a64f937840db",
+    "revision": "9b1143419530a57240960482d70a3c52",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f967c2d7667f0806860bcf4aaea32740.js"
+  "/portfolio/precache-manifest.7ac41d015775e0962e9c8126c7b9471c.js"
 );
 
 workbox.clientsClaim();

c63e4440264f9ab20bb0f79a48d3894f3acb8063
George Marriott <george@tutora.co.uk>
2022-09-02 15:34:21 +0100

    Updates

---
+++ precache-manifest.4937e9b1303553c70477ee519f0ccb6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cb4edf880a509e660ab",
+    "url": "/portfolio/static/css/main.a3d46369.chunk.css"
+  },
+  {
+    "revision": "2cb4edf880a509e660ab",
+    "url": "/portfolio/static/js/main.2cb4edf8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "3039e7852832774707c2",
+    "url": "/portfolio/static/js/2.3039e785.chunk.js"
+  },
+  {
+    "revision": "e84115587a6b718308d376cc5edcb739",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f1e1603698fbce98e68a3e75c3776211.js"
+  "/portfolio/precache-manifest.4937e9b1303553c70477ee519f0ccb6b.js"
 );
 
 workbox.clientsClaim();
